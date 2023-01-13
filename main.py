5cf0d160d4a30cd9f4fa870e1aa3dc56713bbf28
nick <nickgurol@gmail.com>
2023-01-13 07:09:42 -0500

    #create dictionariesrosters dataframe

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 import streamlit as st
 import requests
+import pandas as pd
 def main():
     username = st.text_input('Enter your sleeper username here', 'brochillington')
     user_id = get_user_id(username)
-    get_league_info(user_id)
+    get_rosters(user_id)
 
 def get_user_id(username):
     response = requests.get(f'https://api.sleeper.app/v1/user/{username}')
@@ -12,27 +13,32 @@ def get_user_id(username):
     user_id = data['user_id']
     return user_id
 
-def get_league_info(user_id):
+def get_rosters(user_id):
     current_year = datetime.now().year
     previous_year = current_year-1
     response = requests.get(f'https://api.sleeper.app/v1/user/{user_id}/leagues/nfl/{previous_year}')
     leagues = response.json()
     league_id_list = [d['league_id'] for d in leagues]
     league_name_list = [d['name'] for d in leagues]
-    scoring_settings_list = [d['scoring_settings'] for d in leagues]
-    settings_list = [d['settings'] for d in leagues]
-    roster_positions_list = [d['roster_positions'] for d in leagues]
-    settings_name = dict(zip(league_name_list, settings_list))
-    scoring_settings_name = dict(zip(league_name_list, scoring_settings_list))
-    roster_positions_name = dict(zip(league_name_list, roster_positions_list))
     option = st.selectbox(
         'Please Choose Your League',
         (league_name_list))
+    league_name_id = dict(zip(league_name_list, league_id_list))
+    league_id = league_name_id[option]
+    response = requests.get(f'https://api.sleeper.app/v1/league/{league_id}/rosters')
+    rosters = response.json()
+    response = requests.get(f'https://api.sleeper.app/v1/league/{league_id}/users')
+    users = response.json()
+    users_df = pd.DataFrame(users)
+    rosters_df = pd.DataFrame(rosters)
+    rosters_df_trim = rosters_df[['owner_id', 'roster_id', 'players']]
+    users_df_trim = users_df[['user_id', 'display_name']]
+    rosters_df = rosters_df_trim.merge(users_df_trim, left_on='owner_id', right_on='user_id')
+    rosters_df = rosters_df[['display_name', 'players']]
+    rosters_df = rosters_df.explode('players').reset_index(drop=True)
     if option:
-        st.write(settings_name[option])
-        st.write(scoring_settings_name[option])
-        st.write(roster_positions_name[option])
-    return settings_name, scoring_settings_name, roster_positions_name, option , league_id_list, league_name_list
+        st.write(rosters_df)
+    return rosters
 
 
 if __name__ == '__main__':
