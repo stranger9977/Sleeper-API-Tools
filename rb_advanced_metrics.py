afdeb4005df8c64b31f463f87d972d8cc8fe8fc2
nick <nickgurol@gmail.com>
2023-02-26 10:40:26 -0500

    #advanced_rb_metrics

--- rb_advanced_metrics.py
+++ rb_advanced_metrics.py
@@ -21,7 +21,7 @@ from PIL import Image
 import matplotlib.font_manager as font_manager
 
 
-seasons = np.arange(2018,2023).tolist()
+seasons = np.arange(2011,2023).tolist()
 df = nfl.import_pbp_data(seasons)
 
 df.info(verbose=True)
@@ -29,7 +29,7 @@ df.info(verbose=True)
 roster_data = pd.DataFrame()
 
 # Loop over the past 5 years (including the current year)
-for year in range(2022, 2016, -1):
+for year in range(2022, 2011, -1):
     # Import the roster data for the current year
     roster_df = nfl.import_rosters([year])
 
@@ -38,102 +38,107 @@ for year in range(2022, 2016, -1):
 
 roster_df = nfl.import_rosters([2022])
 roster_df = roster_data
-
-roster_df.info(verbose=True)
-
-roster_df = roster_df[['player_id','player_name','team','position','sleeper_id','headshot_url']]
-
-team_data = nfl.import_team_desc()
-team_df = pd.DataFrame(team_data)
-team_df.columns
-team_df = team_df[['team_abbr', 'team_logo_espn','team_league_logo','team_color','team_color2']]
-player_team = roster_df.merge(team_df, left_on='team', right_on='team_abbr')
-player_team['team_logo_espn'].fillna(player_team['team_league_logo'], inplace=True)
-player_team['player_id'] = player_team['player_id'].astype(str)
-
-player_team['headshot_html'] =  player_team['headshot_url'] \
-        .str.replace(
-        '(.*)',
-        '<img src="\\1" style="max-height:62px;"></img>')
-player_team['team_logo_espn_html'] = player_team['team_logo_espn'] \
-        .str.replace(
-        '(.*)',
-        '<img src="\\1" style="max-height:62px;"></img>')
-rb_roster_df = player_team[player_team['position'] == 'RB'].drop('position',axis = 1)
-
-
-# df = df.dropna(subset=['player_id'])
-
-# df = df.merge(roster, on='player_id')
-# df.info(verbose=True)
-# weighted_opportunity_df = df[['player_id','player_name','receiver_player_id','game_id','pass_attempt','rush_attempt','yardline_100']]
-# plays_per_game_df = df[['player_id','player_name','game_id']]
-# goal_to_go_att = df[['player_id','player_name','game_id','rush_attempt','goal_to_go']]
-# team_tgt = df[['player_id','player_name','posteam','pass_attempt']]
-
-
-rb_roster_df
-
-
-
-df = df.groupby('receiver_player_id').filter(lambda x: x['game_id'].nunique() >= 6)
-
-target_df = df.groupby(['game_id', 'receiver_player_id', 'receiver_player_name', 'posteam'], as_index=False)['pass_attempt'].sum().merge(df.loc[df['receiver_player_id'].notnull()].groupby(['game_id', 'posteam'], as_index=False)['pass_attempt'].sum(), on=['game_id', 'posteam'], suffixes=('_player', '_team'))
-
-target_df['target_share'] = round(target_df['pass_attempt_player'] / target_df['pass_attempt_team'] * 100, 2)
-
-target_df = target_df.groupby(['receiver_player_id', 'receiver_player_name', 'posteam'], as_index=False)[['target_share' , 'pass_attempt_player']].mean().sort_values(by='target_share', ascending=False)
-target_df
-target_df.rename(columns={'receiver_player_id':'player_id', 'posteam':'team','pass_attempt_player':'player_targets','pass_attempt_team':'team_targets'}, inplace=True)
-target_df
-target_df = target_df[['player_id','team','target_share']]
-
-target_df = rb_roster_df.merge(target_df, on=('player_id','team'))
-target_df = target_df.drop_duplicates(subset=['player_id', 'team','target_share'])
-target_df.sort_values(by='target_share', ascending=False,inplace=True)
-target_df.columns
-target_df.rename(columns={'player_name':"Name",'team_logo_espn':'Team', 'target_share':'Team TGT %'}, inplace=True)
-
-
-visual_df = target_df[['Name','headshot_url','Team','Team TGT %','team_color','team_color2']]
-visual_df.to_csv('visual_df.csv', index=False)
-
-# Load the processed data from a file
-
-
-# visual_df=visual_df[:25]
+print(roster_df.columns)
+# roster_df.info(verbose=True)
 #
-# visual_df = visual_df.reset_index(drop=True)
-
-
-# rb_html = (visual_df
-#  .style\
-#  .format({
-#      'Team TGT %': '{:.2f}%'.format
-#      })\
-# .set_properties(**{'text-align':'center','font':'Bebas Neue','font-weight':'bold','font-size':'16px','padding':'5px'})\
-# # .set_table_styles([{'selector': 'th', 'props': [('background-color', '#30b5fd'),('font-size','20px'),('color','white'),('padding','5px')]}])\
-#  .hide_index()\
-#  .render()
-#  )
+# roster_df = roster_df[['player_id','player_name','team','position','sleeper_id','headshot_url']]
 #
-# import os
+# team_data = nfl.import_team_desc()
+# team_df = pd.DataFrame(team_data)
 #
-# # to open/create a new html file in the write mode
-# f = open('rb_html.html', 'w')
+# team_df = team_df[['team_abbr', 'team_logo_espn','team_league_logo','team_color','team_color2']]
+# player_team = roster_df.merge(team_df, left_on='team', right_on='team_abbr')
+# player_team['team_logo_espn'].fillna(player_team['team_league_logo'], inplace=True)
+# player_team['player_id'] = player_team['player_id'].astype(str)
 #
-# # the html code which will go in the file GFG.html
-#
-# # writing the code into the file
-# f.write(rb_html)
-#
-# # close the file
-# f.close()
-#
-# # 1st method how to open html files in chrome using
-# filename = 'file:///'+os.getcwd()+'/' + 'rb_html.html'
+# player_team['headshot_html'] =  player_team['headshot_url'] \
+#         .str.replace(
+#         '(.*)',
+#         '<img src="\\1" style="max-height:62px;"></img>')
+# player_team['team_logo_espn_html'] = player_team['team_logo_espn'] \
+#         .str.replace(
+#         '(.*)',
+#         '<img src="\\1" style="max-height:62px;"></img>')
+# rb_roster_df = player_team[player_team['position'] == 'RB'].drop('position',axis = 1)
 #
 #
-# import IPython
-# IPython.display.HTML(filename='/content/rb_html.html')
-
+# # df = df.dropna(subset=['player_id'])
+#
+# # df = df.merge(roster, on='player_id')
+# # df.info(verbose=True)
+# # weighted_opportunity_df = df[['player_id','player_name','receiver_player_id','game_id','pass_attempt','rush_attempt','yardline_100']]
+# # plays_per_game_df = df[['player_id','player_name','game_id']]
+# # goal_to_go_att = df[['player_id','player_name','game_id','rush_attempt','goal_to_go']]
+# # team_tgt = df[['player_id','player_name','posteam','pass_attempt']]
+#
+# df['goal_to_go_attempt'] = 0
+#
+#
+# # update the values in the new column based on the conditions
+#
+# df.loc[(df['goal_to_go'] == True) & (df['rush_attempt'] == 1), 'goal_to_go_attempt'] = 1
+#
+# goal_df = df.groupby(['game_id', 'rusher_player_id', 'rusher_player_name', 'posteam'], as_index=False)['goal_to_go_attempt'].sum()
+#
+# goal_df = goal_df.groupby(['rusher_player_id', 'rusher_player_name', 'posteam'], as_index=False)[['goal_to_go_attempt']].sum().sort_values(by='goal_to_go_attempt', ascending=False)
+#
+# df = df.groupby('receiver_player_id').filter(lambda x: x['game_id'].nunique() >= 6)
+# #
+# target_df = df.groupby(['game_id', 'receiver_player_id', 'receiver_player_name', 'posteam'], as_index=False)['pass_attempt'].sum().merge(df.loc[df['receiver_player_id'].notnull()].groupby(['game_id', 'posteam'], as_index=False)['pass_attempt'].sum(), on=['game_id', 'posteam'], suffixes=('_player', '_team'))
+# target_df['target_share'] = round(target_df['pass_attempt_player'] / target_df['pass_attempt_team'] * 100, 2)
+# target_df = target_df.groupby(['receiver_player_id', 'receiver_player_name', 'posteam'], as_index=False)[['target_share' , 'pass_attempt_player']].mean().sort_values(by='target_share', ascending=False)
+# target_df.rename(columns={'receiver_player_id':'player_id', 'posteam':'team','pass_attempt_player':'player_targets','pass_attempt_team':'team_targets'}, inplace=True)
+# goal_df.rename(columns={'rusher_player_id':'player_id', 'posteam':'team','goal_to_go_attempt':'goal_to_go_attempts'}, inplace=True)
+#
+# target_df = target_df[['player_id','team','target_share']]
+# goal_df = goal_df[['player_id','team','goal_to_go_attempts']]
+#
+# target_df = target_df.merge(goal_df, on=('player_id','team'))
+# target_df = rb_roster_df.merge(target_df, on=('player_id','team'))
+# target_df = target_df.drop_duplicates(subset=['player_id', 'team','target_share'])
+# target_df.sort_values(by='goal_to_go_attempts', ascending=False,inplace=True)
+# target_df.rename(columns={'player_name':"Name",'team_logo_espn':'Team', 'target_share':'Team TGT %', 'goal_to_go_attempts': 'Goal to Go ATTs'}, inplace=True)
+#
+#
+# visual_df = target_df[['Name','headshot_url','Team','Team TGT %','Goal to Go ATTs','team_color','team_color2']]
+# visual_df.to_csv('visual_df.csv', index=False)
+# #
+# # # Load the processed data from a file
+# #
+# #
+# # # visual_df=visual_df[:25]
+# # #
+# # # visual_df = visual_df.reset_index(drop=True)
+# #
+# #
+# # # rb_html = (visual_df
+# # #  .style\
+# # #  .format({
+# # #      'Team TGT %': '{:.2f}%'.format
+# # #      })\
+# # # .set_properties(**{'text-align':'center','font':'Bebas Neue','font-weight':'bold','font-size':'16px','padding':'5px'})\
+# # # # .set_table_styles([{'selector': 'th', 'props': [('background-color', '#30b5fd'),('font-size','20px'),('color','white'),('padding','5px')]}])\
+# # #  .hide_index()\
+# # #  .render()
+# # #  )
+# # #
+# # # import os
+# # #
+# # # # to open/create a new html file in the write mode
+# # # f = open('rb_html.html', 'w')
+# # #
+# # # # the html code which will go in the file GFG.html
+# # #
+# # # # writing the code into the file
+# # # f.write(rb_html)
+# # #
+# # # # close the file
+# # # f.close()
+# # #
+# # # # 1st method how to open html files in chrome using
+# # # filename = 'file:///'+os.getcwd()+'/' + 'rb_html.html'
+# # #
+# # #
+# # # import IPython
+# # # IPython.display.HTML(filename='/content/rb_html.html')
+# #
--- rushing_metric_visuals.py
+++ rushing_metric_visuals.py
@@ -1,61 +1,3 @@
-
-import nfl_data_py as nfl
-import pprint as pp
-import pandas as pd
-import numpy as np
-import requests
-import os
-import io
-from io import BytesIO
-from PIL import Image
-import seaborn as sns
-import matplotlib.pyplot as plt
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox
-import matplotlib.font_manager as font_manager
-import matplotlib as mpl
-
-# Load the data
-visual_df = pd.read_csv('visual_df.csv')
-
-# Sort the dataframe by Team TGT %
-sorted_df = visual_df.sort_values(by='Team TGT %', ascending=False).head(15)
-sorted_df['label'] = sorted_df['headshot_url'].apply(lambda x: x.split('/')[-1])
-
-for label in sorted_df['label']:
-    response = requests.get(f"https://static.www.nfl.com/image/private/f_auto,q_auto/league/{label}")
-    img = Image.open(BytesIO(response.content))
-
-    # Save the image and get the path
-    img_path = f"image_directory/{label.split('/')[-1]}.png"
-    img.save(img_path, quality=90)
-
-    # Add the path to the DataFrame
-    sorted_df.loc[sorted_df['label'] == label, 'image_path'] = img_path
-font_path = "/Users/nick/sleepertoolsversion2/Exo/static/Exo-Bold.ttf"
-exo_regular = font_manager.FontProperties(fname=font_path)
-
-fig, ax = plt.subplots(figsize=(15, 10))
-
-def getImage(path):
-    return OffsetImage(plt.imread(path), zoom=.030)
-
-# Add team logos
-for y0, x0, path in zip(sorted_df['Name'], sorted_df['Team TGT %'] + .005, sorted_df['image_path']):
-    ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False, fontsize=4)
-    ax.add_artist(ab)
-
-# Add bar chart, y axis is an array from 0-31 (length of 32, one per team)
-ax.barh(sorted_df['Name'], sorted_df['Team TGT %'], color=sorted_df['team_color'], height=0.5, edgecolor=sorted_df['team_color2'],
-        linewidth=3)
-
-# Add a grid across the x-axis
-ax.grid(zorder=0, alpha=.5, axis='x', linewidth=3)
-ax.set_axisbelow(True)
-ax.axes.xaxis.set_visible(False)
-
-# Invert y-axis
-ax.invert_yaxis()
-
 import nfl_data_py as nfl
 import pprint as pp
 import pandas as pd
@@ -72,69 +14,100 @@ import matplotlib.font_manager as font_manager
 
 # Load the data
 visual_df = pd.read_csv('visual_df.csv')
+metric_list = ['Team TGT %']
+visual_df = visual_df.replace('nan', np.nan)
 
-# Sort the dataframe by Team TGT %
-sorted_df = visual_df.sort_values(by='Team TGT %', ascending=False).head(15)
-sorted_df['label'] = sorted_df['headshot_url'].apply(lambda x: x.split('/')[-1])
+visual_df = visual_df.dropna(subset = ['headshot_url'])
+for metric in metric_list:
 
-for label in sorted_df['label']:
-    response = requests.get(f"https://static.www.nfl.com/image/private/f_auto,q_auto/league/{label}")
-    img = Image.open(BytesIO(response.content))
+    # Sort the dataframe by Team TGT %
 
-    # Save the image and get the path
-    img_path = f"image_directory/{label.split('/')[-1]}.png"
-    img.save(img_path, quality=90)
+    sorted_df = visual_df.sort_values(by=metric, ascending=False).head(15)
 
-    # Add the path to the DataFrame
-    sorted_df.loc[sorted_df['label'] == label, 'image_path'] = img_path
-font_path = "/Users/nick/sleepertoolsversion2/Exo/static/Exo-Bold.ttf"
-exo_regular = font_manager.FontProperties(fname=font_path)
+    sorted_df['label'] = sorted_df['headshot_url'].apply(lambda x: x.split('/')[-1]).astype(str).dropna()
 
-fig, ax = plt.subplots(figsize=(15, 10))
+    for label in sorted_df['label']:
+        response = requests.get(f"https://static.www.nfl.com/image/private/f_auto,q_auto/league/{label}")
+        img = Image.open(BytesIO(response.content))
 
-def getImage(path):
-    return OffsetImage(plt.imread(path), zoom=.035)
+        # Save the image and get the path
+        img_path = f"image_directory/{label.split('/')[-1]}.png"
+        img.save(img_path, quality=90)
 
-# Add team logos
-for y0, x0, path in zip(sorted_df['Name'], sorted_df['Team TGT %'] + .005, sorted_df['image_path']):
-    ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False, fontsize=4)
-    ax.add_artist(ab)
+        # Add the path to the DataFrame if img_path is not NaN
+        if img_path != 'NaN':
+            sorted_df.loc[sorted_df['label'] == label, 'image_path'] = img_path
 
-# Add bar chart, y axis is an array from 0-31 (length of 32, one per team)
-ax.barh(sorted_df['Name'], sorted_df['Team TGT %'], color=sorted_df['team_color'], height=0.5, edgecolor=sorted_df['team_color2'],
-        linewidth=3)
+    font_path = "/Users/nick/sleepertoolsversion2/Exo/static/Exo-Bold.ttf"
 
-# Invert y-axis
-ax.invert_yaxis()
-ax.set_xlim(10, 25)
-# Set the font for the axis and plot titles
-ax.set_xlabel('Team TGT %', fontproperties=exo_regular, fontsize=18, labelpad=20, color='#FCC331')
-ax.set_facecolor('#202F52')
-fig.patch.set_facecolor('#202F52')
-ax.set_title('Top 15 RBs 2018-2023: Team TGT % ', fontproperties=exo_regular, fontsize=26, pad=20, fontweight='bold', loc='left', color='#FCC331')
-for i, v in enumerate(sorted_df['Team TGT %']):
-    ax.text(v + .50, i - 0.25, '{:.01f}%'.format(v), color='#FCC331', fontproperties=exo_regular)
+    exo_regular = font_manager.FontProperties(fname=font_path)
 
-# Set the color of the tick marks to #FCC331
-ax.tick_params(axis='both', colors='#FCC331')
+    fig, ax = plt.subplots(figsize=(15, 10))
 
-# Set the color of the border of the plot to #FCC331
-for spine in ax.spines.values():
-    spine.set_edgecolor('#FCC331')
+    def getImage(path):
+        return OffsetImage(plt.imread(str(path)), zoom=.035)
 
-# Set the background color of the figure
-fig.patch.set_facecolor('#202F52')
-plt.figtext(.65, .06, 'Figure: @run_the_sims | Data: nflfastR', fontsize=14, fontproperties=exo_regular, color='#FCC331')
-plt.show()
+    # Add team logos
 
-plt.savefig('/Users/nick/sleepertoolsversion2/team_target_%rb.png', dpi=300, bbox_inches='tight', )
+    for y0, x0, path in zip(sorted_df['Name'], sorted_df[metric] + .005, sorted_df['image_path']):
 
+        ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False, fontsize=4)
 
+        ax.add_artist(ab)
 
+    # Add bar chart, y axis is an array from 0-31 (length of 32, one per team)
 
+    ax.barh(sorted_df['Name'], sorted_df[metric], color=sorted_df['team_color'], height=0.5, edgecolor=sorted_df['team_color2'],
 
+            linewidth=3)
 
+    # Invert y-axis
 
+    ax.invert_yaxis()
 
+    min_value = sorted_df[metric].min()
+    max_value = sorted_df[metric].max()
 
+    # Calculate the range for the xlim
+    xlim_range = max_value - min_value
 
+    # Set the xlim for the plot
+    ax.set_xlim(min_value - 2, max_value + 2)
+
+    # Set the font for the axis and plot titles
+
+    ax.set_xlabel(metric, fontproperties=exo_regular, fontsize=18, labelpad=20, color='#FCC331')
+
+    ax.set_facecolor('#202F52')
+
+    fig.patch.set_facecolor('#202F52')
+    ax.set_title(f'Top 15 RBs: {metric} 2011-2022', fontproperties=exo_regular, fontsize=26,pad=20, fontweight='bold', loc='left', color='#FCC331')
+    if '%' in metric:
+        # Set the format string for percentage values
+        format_str = '{:.02f}%'
+    else:
+        # Set the format string for non-percentage values
+        format_str = '{:.0f}'
+    for i, v in enumerate(sorted_df[metric]):
+
+        ax.text(v + .80, i - 0.25, format_str.format(v), color='#FCC331', fontproperties=exo_regular)
+
+    # Set the color of the tick marks to #FCC331
+
+    ax.tick_params(axis='both', colors='#FCC331')
+
+    # Set the color of the border of the plot to #FCC331
+
+    for spine in ax.spines.values():
+
+        spine.set_edgecolor('#FCC331')
+
+    # Set the background color of the figure
+
+    fig.patch.set_facecolor('#202F52')
+
+    plt.figtext(.65, .06, 'Figure: @run_the_sims | Data: nflfastR', fontsize=14, fontproperties=exo_regular, color='#FCC331')
+
+    plt.savefig(f'/Users/nick/sleepertoolsversion2/{metric}.png', dpi=300, bbox_inches='tight')
+
+    plt.show()
